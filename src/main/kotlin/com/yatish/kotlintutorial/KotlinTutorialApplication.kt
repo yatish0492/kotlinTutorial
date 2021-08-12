50dedf5016e9fdb4d500e6f789e1124291e29e64
Yatish C S <ycs@hotels.com>
2021-08-12 19:43:48 +0530

    Initial commit spring boot kotlin skeleton project

---
+++ src/main/kotlin/com/yatish/kotlintutorial/KotlinTutorialApplication.kt
@@ -0,0 +1,11 @@
+package com.yatish.kotlintutorial
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinTutorialApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinTutorialApplication>(*args)
+}
